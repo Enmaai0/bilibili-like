c57007fdc1363fe5780963667a2a57cf98d177f4
Zhang Hao <vxyu82505@gmail.com>
2025-05-06 21:57:01 +1000

    Implemented uploaded file by slices.

--- bilibili-api/src/main/java/com/bilibili/api/FileApi.java
+++ bilibili-api/src/main/java/com/bilibili/api/FileApi.java
@@ -4,7 +4,6 @@ import com.bilibili.domain.JsonResponse;
 import com.bilibili.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.multipart.MultipartFile;
