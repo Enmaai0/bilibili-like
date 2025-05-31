b91c7ada6b4925647c5091d8fee8ccf25c3dbe8e
Zhang Hao <vxyu82505@gmail.com>
2025-06-01 05:18:17 +1000

    Implemented elasticsearch for userInfo

--- bilibili-service/src/main/java/com/bilibili/service/ElasticService.java
+++ bilibili-service/src/main/java/com/bilibili/service/ElasticService.java
@@ -1,21 +1,35 @@
 package com.bilibili.service;
 
+import co.elastic.clients.elasticsearch.core.SearchRequest;
+import com.bilibili.dao.repository.UserInfoRepository;
 import com.bilibili.dao.repository.VideoRepository;
+import com.bilibili.domain.UserInfo;
 import com.bilibili.domain.Video;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class ElasticService {
     @Autowired
     private VideoRepository videoRepository;
 
-    public void add(Video video) {
+    @Autowired
+    private UserInfoRepository userInfoRepository;
+
+    public void addVideo(Video video) {
         videoRepository.save(video);
     }
 
     public Video getVideos(String keyword) {
         Video video = videoRepository.findByTitleLike(keyword);
         return video;
+    }
+
+    public void addUserInfo(UserInfo userInfo) {
+        userInfoRepository.save(userInfo);
     }
 }
